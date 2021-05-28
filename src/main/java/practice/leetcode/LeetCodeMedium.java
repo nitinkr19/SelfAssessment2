913bccd674325b5cd34ad6ee6527d3940180e17e
Nitin Kumar Srivastava <nitin@ezcred.in>
2021-05-28 12:54:08 +0530

    SELF-1|added cake cuts max area problem

--- src/main/java/practice/leetcode/LeetCodeMedium.java
+++ src/main/java/practice/leetcode/LeetCodeMedium.java
@@ -1,5 +1,7 @@
 package main.java.practice.leetcode;
 
+import java.util.Arrays;
+
 public class LeetCodeMedium {
 
   /*
@@ -61,6 +63,25 @@ public class LeetCodeMedium {
       }
     }
     return (x == 0 && y == 0) || ind != 0;
+  }
+
+  public int maxAreaInCakeCuts(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+
+    Arrays.sort(horizontalCuts);
+    Arrays.sort(verticalCuts);
+
+    int hMaxDiff = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length-1]);
+    int vMaxDiff = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length-1]);
+
+    for (int i = 1; i < horizontalCuts.length; i++) {
+      hMaxDiff = Math.max(hMaxDiff, horizontalCuts[i] - horizontalCuts[i - 1]);
+    }
+
+    for (int i = 1; i < verticalCuts.length; i++) {
+      vMaxDiff = Math.max(vMaxDiff, verticalCuts[i] - verticalCuts[i - 1]);
+    }
+
+    return hMaxDiff * vMaxDiff;
   }
 
 }
--- src/main/java/practice/leetcode/LeetCodeMediumTest.java
+++ src/main/java/practice/leetcode/LeetCodeMediumTest.java
@@ -16,4 +16,12 @@ class LeetCodeMediumTest {
     assertEquals(true, leetCodeMedium.isRobotBounded("GL"));
     assertEquals(true, leetCodeMedium.isRobotBounded("GLRLLGLL"));
   }
+
+  @Test
+  void maxAreaInCakeCuts() {
+
+    assertEquals(9,leetCodeMedium.maxAreaInCakeCuts(5,4,new int[]{3}, new int[]{3}));
+    assertEquals(4,leetCodeMedium.maxAreaInCakeCuts(5,4,new int[]{1,2,4}, new int[]{1,3}));
+    assertEquals(6,leetCodeMedium.maxAreaInCakeCuts(5,4,new int[]{3,1}, new int[]{1}));
+  }
 }
